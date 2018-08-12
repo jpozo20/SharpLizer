f46359fb85926fcda65a2d15bc5ced362a6daf62
Jefry Pozo <jefrypozo@outlook.com>
2018-08-12 12:31:04 -0400

    Add color for declaration types
    -This includes class, interface, enum, struct among others
    -Also change the priority of colors so they are completely applied

--- SharpLizer/Classification/ColorFormats.cs
+++ SharpLizer/Classification/ColorFormats.cs
@@ -13,7 +13,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.FieldType)]
     [Name(ClassificationTypes.FieldType)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class FieldTypeClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -32,7 +32,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.MethodType)]
     [Name(ClassificationTypes.MethodType)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class MethodTypeClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -47,11 +47,13 @@ namespace SharpLizer.Classification
         }
     }
 
+    #region Abstraction Keywords Colors
+
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.AbstractionKeywords)]
     [Name(ClassificationTypes.AbstractionTypes.AbstractionKeywords)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class AbstractionKeywordsClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -70,7 +72,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.AbstractKeyword)]
     [Name(ClassificationTypes.AbstractionTypes.AbstractKeyword)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class AbstractKeywordClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -89,7 +91,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.AsyncKeyword)]
     [Name(ClassificationTypes.AbstractionTypes.AsyncKeyword)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class AsyncKeywordClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -108,7 +110,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.NewKeyword)]
     [Name(ClassificationTypes.AbstractionTypes.NewKeyword)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class NewKeywordClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -127,7 +129,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.OverrideKeyword)]
     [Name(ClassificationTypes.AbstractionTypes.OverrideKeyword)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class OverrideKeywordClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -146,7 +148,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.SealedKeyword)]
     [Name(ClassificationTypes.AbstractionTypes.SealedKeyword)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class SealedKeywordClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -165,7 +167,7 @@ namespace SharpLizer.Classification
     [ClassificationType(ClassificationTypeNames = ClassificationTypes.AbstractionTypes.VirtualKeyword)]
     [Name(ClassificationTypes.AbstractionTypes.VirtualKeyword)]
     [UserVisible(true)] // This should be visible to the end user
-    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
     internal sealed class VirtualKeywordClassification : ClassificationFormatDefinition
     {
         /// <summary>
@@ -179,4 +181,137 @@ namespace SharpLizer.Classification
             this.TextDecorations = System.Windows.TextDecorations.Underline;
         }
     }
+
+    #endregion
+
+    #region Declaration Keywords Colors
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.DeclarationKeywords)]
+    [Name(ClassificationTypes.DeclarationTypes.DeclarationKeywords)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class DeclarationKeywordsClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeclarationKeywordsClassification"/> class.
+        /// </summary>
+        public DeclarationKeywordsClassification()
+        {
+            this.DisplayName = "SharpLizer: Class Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.Yellow;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.ClassKeyword)]
+    [Name(ClassificationTypes.DeclarationTypes.ClassKeyword)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class ClassKeywordClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassKeywordClassification"/> class.
+        /// </summary>
+        public ClassKeywordClassification()
+        {
+            this.DisplayName = "SharpLizer: Class Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.Red;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.DelegateKeyword)]
+    [Name(ClassificationTypes.DeclarationTypes.DelegateKeyword)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class DelegateKeywordClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateKeywordClassification"/> class.
+        /// </summary>
+        public DelegateKeywordClassification()
+        {
+            this.DisplayName = "SharpLizer: Delegate Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.YellowGreen;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.EnumKeyword)]
+    [Name(ClassificationTypes.DeclarationTypes.EnumKeyword)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class EnumKeywordClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumKeywordClassification"/> class.
+        /// </summary>
+        public EnumKeywordClassification()
+        {
+            this.DisplayName = "SharpLizer: Enum Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.Tomato;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.InterfaceKeyword)]
+    [Name(ClassificationTypes.DeclarationTypes.InterfaceKeyword)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class InterfaceKeywordClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InterfaceKeywordClassification"/> class.
+        /// </summary>
+        public InterfaceKeywordClassification()
+        {
+            this.DisplayName = "SharpLizer: Interface Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.Violet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.NamespaceKeyword)]
+    [Name(ClassificationTypes.DeclarationTypes.NamespaceKeyword)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class NamespaceKeywordClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NamespaceKeywordClassification"/> class.
+        /// </summary>
+        public NamespaceKeywordClassification()
+        {
+            this.DisplayName = "SharpLizer: Namespace Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.PaleGreen;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = ClassificationTypes.DeclarationTypes.StructKeyword)]
+    [Name(ClassificationTypes.DeclarationTypes.StructKeyword)]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] // Set the priority to be after the default classifiers
+
+    internal sealed class StructKeywordClassification : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StructKeywordClassification"/> class.
+        /// </summary>
+        public StructKeywordClassification()
+        {
+            this.DisplayName = "SharpLizer: Struct Keyword"; // Human readable version of the name
+            this.ForegroundColor = Colors.MediumVioletRed;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    #endregion
 }
