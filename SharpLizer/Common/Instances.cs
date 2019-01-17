7f99de9f373c8289fd2d8e535948a331c83b29e8
Jefry Pozo <jefrypozo@outlook.com>
2019-01-16 21:50:20 -0400

    Add MEF dependency injecto to MainViewModel

---
+++ SharpLizer/Common/Instances.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.ComponentModelHost;
+using System;
+
+namespace SharpLizer.Common
+{
+    internal static class Instances
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+        public static IComponentModel CompositionService { get; set; }
+    }
+}
---
+++ SharpLizer/Helpers/ServiceProviderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.ComponentModelHost;
+using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Primitives;
+
+namespace SharpLizer.Helpers
+{
+    internal static class ServiceProviderExtensions
+    {
+        internal static void SatisfyImportsOnce(this IServiceProvider serviceProvider, object objectToCompose)
+        {
+            IComponentModel compositionService = Common.Instances.CompositionService;
+            if (compositionService == null)
+            {
+                compositionService = serviceProvider.GetService<SComponentModel, IComponentModel>();
+            }
+
+            if (compositionService != null)
+            {
+                compositionService.DefaultCompositionService.SatisfyImportsOnce(objectToCompose);
+            }
+        }
+
+        private static T GetService<T>(this IServiceProvider serviceProvider) where T : class
+        {
+            return serviceProvider.GetService(typeof(T)) as T;
+        }
+
+        private static TReturn GetService<TGet, TReturn>(this IServiceProvider serviceProvider)
+            where TGet : class
+            where TReturn : class
+        {
+            return serviceProvider.GetService(typeof(TGet)) as TReturn;
+        }
+    }
+}
